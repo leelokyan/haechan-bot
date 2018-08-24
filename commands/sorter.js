3ec7e1a2f1894d77970134e706bff1c9f1f65c79
Alice Lee <alicesle@usc.edu>
2018-08-23 17:03:03 -0700

    added async stuff to sorter

--- commands/sorter.js
+++ commands/sorter.js
@@ -7,62 +7,108 @@ exports.run = (client, message, args) => {
 	//i could probably separate this out as well? for readability? 
 	
 
-	//const currUser = userID; 
-    message.channel.send('BTS sorter starting...');
-    message.channel.send('Choose wisely, ' + message.author);
+	const currUser = message.author; 
+    message.channel.send('BTS sorter starting...')
+    	.then((message) => {
+			console.log(`Sent message ${message.content}`);
+			return message.channel.send('Choose wisely, ' + currUser);    				
+		})	
+    	.then((message) => {
+    		console.log(`Sent message ${message.content}`);
+    		return fs.readFileSync('./commands/BTS.txt', 'utf8');
+    	})
+    	.then((data) => {
+    		console.log(data);
+	    	let members = data.split('\r\n'); 
+	    	console.log(members);
+	    	//message.channel.send(members);
+	    	//quickSort(members,members,0,members.length);
+	    	return members; 
+    	})
+    	.then((members) => {
+    		return quickSort(members,members,0,members.length);
+    	})
+    	.then(success => console.log(success)); 
+
+    function hi(array) {
+    	message.channel.send(array)
+    		.then(message => console.log(message))
+    		.catch(console.error);
+    };
+
+    async function quickSort(names, result, start, end) {
+		console.log('in quicksort');
+
+    	if ((end - start) <= 1) return;
+    	let pivot = (end + start) / 2;
+    	let lcount = start;
+    	let rcount = end - 1;
+
+    	for (i = start; i < end; i++) {
+    		if (i === pivot) continue;
+    		message.channel.send(`${names[pivot]} or ${names[i]}?\n
+    			Enter 0 for ${names[pivot]} or 1 for ${names[i]}.`)
+    		.then((msg) => {
+    			let collectedMsg; 
+    			const collector = new Discord.MessageCollector(message.channel, 
+    				m => m.author.id === message.author.id, {time: 10000 });
+    			collector.on('collect', message => {
+    				collectedMsg = message;
+	    			
+    			});
+    			return collectedMsg;
+    		})
+    		.then(message => {
+    			if (message.content == 1) {
+    				result[lcount] = names[i];
+    				lcount++;
+    				collector.close();
+    			} else if (message.content == 0) {
+    				result[rcount] = names[i];
+    				rcount--;
+    				collector.close();
+    			} else {
+
+    			}
+    			return message.content;
+    		})
+    		.then((content) => {
+    			result[lcount] = names[pivot];
+    			names = result;
+    			quickSort(names,result,start,pivot)
+    			.then((success) => {
+    				if (success)
+    				{
+    					quickSort(names,result,pivot + 1, end);
+    				}
+    				else {
+    					return false;
+    				}
+    			})
+    			 
+    		})
+    		.catch((err) => {return false;})
+    		
+		}
+		return true;
+    };
     
-    fs.readFile('./commands/BTS.txt', 'utf8', (err, data) => {
+    
+
+    /* fs.readFile('./commands/BTS.txt', 'utf8', (err, data) => {
     	if (err) console.log('Failed to read file');
     	console.log(data);
     	let members = data.split('\r\n'); 
     	console.log(members);
     	message.channel.send(data);
-    	quickSort(members,members,0,members.length);
+    	//quickSort(members,members,0,members.length);
 
     	console.log(members);
     	
-    	
-    });
-
-    let quickSort = function(names, result, start, end) {
-    		console.log('in quicksort');
-    		throw new Error("sldkjsld");
-    		message.channel.send('in quicksort');
-	    	if ((end - start) <= 1) return;
-	    	let pivot = (end + start) / 2;
-	    	let lcount = start;
-	    	let rcount = end - 1;
-
-	    	for (i = start; i < end; i++) {
-	    		if (i === pivot) continue;
-	    		message.channel.send(`${names[pivot]} or ${names[i]}?\n
-	    			Enter 0 for ${names[pivot]} or 1 for ${names[i]}.`);
-	    		const collector = new Discord.MessageCollector(message.channel, 
-	    			m => m.author.id === message.author.id, {time: 10000 });
-	    		collector.on('collect', message => {
-	    			if (message.content == 1) {
-	    				result[lcount] = names[i];
-	    				lcount++;
-	    				collector.close();
-	    			} else if (message.content == 0) {
-	    				result[rcount] = names[i];
-	    				rcount--;
-	    				collector.close();
-	    			} else {
-
-	    			}
-
-	    		});
-
-    		result[lcount] = names[pivot];
-
-    		names = result; 
-    		quickSort(names,result,start,pivot);
-    		quickSort(names,result,pivot + 1, end);
-    	}
-    };
-
-    	
+    	hi(); 
+	    message.channel.send("done");
+	    console.log('done');
+    });*/
 
 }
 
